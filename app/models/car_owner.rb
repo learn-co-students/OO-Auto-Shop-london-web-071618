16e34b76ebaadc02fc1a477878849ffbae40da8b
ritz-w <ritz.wu@outlook.com>
2018-07-27 17:55:20 +0100

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,26 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def cars
+    Car.select {|car| car.car_owner = self}
+  end
+
+  def mechanics
+    self.cars.map {|x| x.mechanic}
+  end
+
+  def self.avg_of_all
+      all_cars = self.all.map {|owner| owner.cars.size }
+      all_cars.inject(:+) / all_cars.size 
+  end
+
 
 
 end
