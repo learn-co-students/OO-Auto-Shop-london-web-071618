e81336f7c397a8e7a4ee21b2b741ec91ac95afa1
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-07-29 21:05:47 +0100

    Jo's Solution

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,28 @@
 class Car
 
-  attr_reader :make, :model
+@@all = []
 
-  def initialize(make, model, classification)
+  attr_reader :make, :model, :classification, :car_owner, :mechanic
+
+  def initialize(make, model, classification, car_owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @car_owner = car_owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    @@all.map {|car| car.classification}.uniq
+  end
+
+  def mechanics_matching_classification_expertise
+      Mechanic.all.select {|mechanic|  mechanic.specialty == self.classification}
   end
 
 end
